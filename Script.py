88e7694174d7c87328a7816f19b84ed30c9a2094
William Clark <willowlark@outlook.com>
2016-10-18 21:20:18 -0400

    Leveling implemented

--- Script.py
+++ Script.py
@@ -3,6 +3,7 @@ from random import randint
 
 global scope
 
+
 class CsvImport(object):
     def __init__(self, d):
         for a, b in d.items():
@@ -10,6 +11,7 @@ class CsvImport(object):
                setattr(self, a, [CsvImport(x) if isinstance(x, dict) else x for x in b])
             else:
                setattr(self, a, CsvImport(b) if isinstance(b, dict) else b if not b.isdigit() else int(b))
+
 
 class Unit(CsvImport):
 
@@ -25,6 +27,16 @@ class Unit(CsvImport):
         self.das = None
 
         self.tryEquip()
+
+    def __str__(self):
+        if self.using is not None: output = """{0}: {1}/{2}\nClass: {3} {4} ({5})\nUsing: {6}.
+        """.format(self.Name, self.CHP, self.HP, self.Class, self.Lv, self.Exp, self.using.Name)
+        else: output = """{0}: {1}/{2}\nClass: {3} {4} ({5}).
+        """.format(self.Name, self.CHP, self.HP, self.Class, self.Lv, self.Exp)
+        return output
+
+    def __repr__(self):
+        return self.writeString()
 
     def tryEquip(self):
         global scope
@@ -53,13 +65,36 @@ class Unit(CsvImport):
         else:
             print "Has nothing to use."
 
-    #Name,Class,Lv,Exp,HP,Str,Mag,Skl,Spd,Luck,Def,Res,Con,Mov,Affinity,I1,,,,,,,,Usable Weapons,
+    def levelUp(self):
+
+        # if self.Exp < 100:
+        #     print "Not enough exp to level up!"
+        #     return -1
+        #
+        # if self.Lv == 20:
+        #     print "Max level reached."
+        #     return -2
+
+        self.Lv += 1
+        self.Exp = 0
+
+        # Horrible not a good idea code yay!
+        for attr, value in self.__dict__.iteritems():
+            if attr in ["GHP", 'GStr', 'GMag', 'GSkl', 'GSpd', 'GLuck','GDef', 'GRes']:
+                r = randint(1, _roundup(value))
+                bonus = r/ 100 + 1
+                if r < value:
+                    self.__setattr__(attr[1:], self.__getattribute__(attr[1:])+bonus)
+                    if attr[1:] == "HP": self.CHP += bonus
+                    print "{0} has increased by {1}!".format(attr[1:], bonus)
+
     def writeString(self):
         ret = [self.Name, self.Class, self.Lv, self.Exp, self.CHP, self.HP, self.Str, self.Mag, self.Skl, self.Spd,
                 self.Luck, self.Def, self.Res, self.Con, self.Mov, self.Affinity, self.I1, self.I2, self.I3, self.I4,
                self.I5, self.I6, self.I7, self.I8, self.Usable, self.GHP, self.GStr, self.GMag, self.GSkl, self.GSpd,
                self.GLuck, self.GDef, self.GRes]
         return ret
+
 
 class PartUnit(CsvImport):
 
@@ -75,6 +110,14 @@ class PartUnit(CsvImport):
         self.das = self.AS
 
         self.tryEquip()
+
+    def __str__(self):
+        output = """{0}: {1}/{2}\nClass: {3} {4}.
+        """.format(self.Name, self.CHP, self.HP, self.Class, self.Lv)
+        return output
+
+    def __repr__(self):
+        return self.writeString()
 
     def tryEquip(self):
         it = Item({})
@@ -98,10 +141,20 @@ class PartUnit(CsvImport):
                self.Avd, self.Crt, self.Evd, self.AS, self.Mov]
         return ret
 
+
 class Enemy(Unit):
     pass
 
+
 class Item(CsvImport):
+
+    def __str__(self):
+        output = """{0}\nMt: {1} Crit: {2} Rng: {3}\n{4}
+        """.format(self.Name, self.Mt, self.Crt, self.Range, self.Effect)
+        return output
+
+    def __repr__(self):
+        return self.writeString()
 
     def writeString(self):
         #Name,Mt,Hit,Crt,Weight,Range,Rank,Effect,Physical
@@ -121,7 +174,7 @@ def fight(command):
         print "Someone isn't equipped"
         return -1
 
-    #Calculate combat results
+    # Calculate combat results
     adamage = attacker.dattack - defender.Def if defender.using.Physical == 'TRUE' else defender.Res
     aaccuracy = attacker.dhit - defender.davd
     acrt = attacker.dcrt - defender.devd
@@ -131,10 +184,10 @@ def fight(command):
     daccuracy = defender.dhit - attacker.davd
     dcrt = defender.dcrt - attacker.devd
     ddouble = defender.das - attacker.das >= 4
-    #confirm attack
+
     data = [['Name', 'Damage', 'To Hit', 'Crit Chance', 'Double?'], [attacker.Name, adamage, aaccuracy, acrt, adouble],
             [defender.Name, ddamage, daccuracy, dcrt, ddouble]]
-    col_width = 10 #max(len(word) for row in data for word in row) + 2  # padding
+    col_width = 10  # max(len(word) for row in data for word in row) + 2  # padding
     for row in data:
         print "".join(str(word).ljust(col_width) for word in row)
     prompt = raw_input("Make the attack?> ")
@@ -149,13 +202,13 @@ def fight(command):
         #do exp.
 
 
-
 def uses(command):
     global scope
     index = command.index('uses')
     unit = " ".join(command[:index])
     item = " ".join(command[index+1:])
     scope[unit].equipped(item)
+
 
 def load(command):
     global scope
@@ -174,6 +227,7 @@ def load(command):
             elif loadtype is 3: scope[row["Name"]] = Enemy(row)
             elif loadtype is 4: scope[row["Name"]] = PartUnit(row)
             else: print 'Error'
+
 
 def save(command):
     global scope
@@ -196,17 +250,16 @@ def save(command):
 
     for key in scope:
         value = scope[key]
-        # if type(value) is Unit and units: writer.writerow(value.writeString())
-        # if type(value) is Item and items: writer.writerow(value.writeString())
-        # if type(value) is Enemy and enemies: writer.writerow(value.writeString())
         if _actcheck(units,items,enemies,punit,value): writer.writerow(value.writeString())
     f.close()
+
 
 def show(command):
     units, items, enemies, punit, _ = _flags(command)
     for key in scope:
         value = scope[key]
         if _actcheck(units,items,enemies,punit,value): print value.Name, value
+
 
 def main():
     global scope
@@ -218,6 +271,8 @@ def main():
                 fight(command)
             elif 'uses' in command:
                 uses(command)
+            elif 'level' in command:
+                scope[" ".join(command[1:])].levelUp()
             elif 'load' in command:
                 load(command)
             elif 'save' in command:
@@ -233,11 +288,16 @@ def main():
                 if " ".join(command) in scope:
                     print scope[' '.join(command)]
                 else:
-                    print "Unrecognized command, object not in scope."
+                    print "Unrecognized command and nothing with that name exists."
         except KeyError:
             print "A name was spelled incorrectly. Capitalization matters."
         except IOError:
             print "A bad file name was used."
+
+
+def _roundup(x):
+    return x if x % 100 == 0 else x + 100 - x % 100
+
 
 def _actcheck(units,items,enemies,punits,value=None):
     #Used in load, save, show.
@@ -246,6 +306,7 @@ def _actcheck(units,items,enemies,punits,value=None):
     if (type(value) is Enemy or value is None) and enemies: return 3
     if (type(value) is PartUnit or value is None) and punits: return 4
     return 0
+
 
 def _flags(command):
     #used in load, save, show.
